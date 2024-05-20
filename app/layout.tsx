1c90d6ccd448a5f85d04d5078e0eb48e650fb546
Kathryn Dionisio <kathryndionisio@Kathryns-Air.attlocal.net>
2024-05-20 09:14:51 -0700

    replace link icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 import { theme } from '../theme';
 
 export const metadata = {
-  title: 'Mantine Next.js template',
-  description: 'I am using Mantine with Next.js!',
+  title: 'JetStream',
+  description: 'A news app powered by Goodwin',
 };
 
 export default function RootLayout({ children }: { children: any }) {
